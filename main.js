43cc8d8ec917716f408a38c3a7b0893043d27ccf
AnuragDubey67 <101169657+AnuragDubey67@users.noreply.github.com>
2023-11-04 18:50:20 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const accessKey = "9LPqLq9HvgK0FNlR8koHRslIQ0LXhhZ37vDwpoJ2YcM";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
+
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.taxtContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    imageWrapper.appendChild(imageWrapper);
+
+
+
+
+  });
+
+  page++
+  if(page > 1){
+    showMore.style.display = "block"
+    
+  }
+}
